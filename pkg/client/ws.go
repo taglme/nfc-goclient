2882b5220a6b47ab917086679f547201875d643f
qwelp <olegobt@mail.ru>
2020-02-27 16:11:33 +0300

    ws draft

--- pkg/client/ws.go
+++ pkg/client/ws.go
@@ -1,6 +1,9 @@
 package client
 
 import (
+	"encoding/json"
+
+	"github.com/gorilla/websocket"
 	"github.com/taglme/nfc-client/pkg/models"
 )
 
@@ -8,18 +11,48 @@ type WsService interface {
 	Connect() error
 	Disconnect() error
 	IsConnected() bool
-	OnTagDiscovery(func(adapterID string, tag models.Tag))
-	OnTagRelease(func(adapterID string, tag models.Tag))
-	OnAdapterDiscovery(func(adapterID string, adapter models.Adapter))
-	OnAdapterRelease(func(adapterID string, adapter models.Adapter))
-	OnRunStarted(func(adapterID string, jobRun models.JobRun))
-	OnRunSuccess(func(adapterID string, jobRun models.JobRun))
-	OnRunError(func(adapterID string, jobRun models.JobRun))
-	OnJobSubmitted(func(adapterID string, job models.Job))
-	OnJobRemoved(func(adapterID string, job models.Job))
-	OnJobActivated(func(adapterID string, job models.Job))
-	OnJobPending(func(adapterID string, job models.Job))
-	OnJobFinished(func(adapterID string, job models.Job))
+	OnEvent(func(handler EventHandler))
 	SetLocale(locale string) error
 	ConnString() string
+}
+
+type EventHandler func(e models.Event)
+type wsService struct {
+	conn     *websocket.Conn
+	handlers []EventHandler
+}
+
+func (ws *wsService) OnEvent(handler EventHandler) {
+	ws.handlers = append(ws.handlers, handler)
+}
+
+func (ws *wsService) eventListener(e models.Event) {
+	for _, handler := range ws.handlers {
+		handler(e)
+	}
+}
+
+func (ws *wsService) read() {
+	defer ws.conn.Close()
+	for {
+		var eventResource models.EventResource
+		_, message, err := ws.conn.ReadMessage()
+		if err != nil {
+			break
+		}
+		err = json.Unmarshal(message, &eventResource)
+		if err != nil {
+			//log here
+			continue
+		}
+
+		event, err := eventResource.ToEvent()
+		if err != nil {
+			//log here
+			continue
+		}
+		ws.eventListener(event)
+
+	}
+
 }
